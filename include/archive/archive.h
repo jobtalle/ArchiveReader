3db7996775bdd8c6ca9e6f07e0ee07a326c9fc8b
jobtalle <jobtalle@hotmail.com>
2017-03-25 13:11:30 +0100

    Added sources

---
+++ include/archive/archive.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class Archive
+{
+public:
+	Archive(const std::string path);
+	~Archive();
+private:
+
+};
---
+++ src/archive/archive.cpp
@@ -0,0 +1,13 @@
+#include <archive/archive.h>
+
+#include <stdexcept>
+
+Archive::Archive(const std::string path)
+{
+
+}
+
+Archive::~Archive()
+{
+
+}
