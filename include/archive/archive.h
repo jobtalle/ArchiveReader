857eb7ca7eaa49c1b734d834afff444c9f8162c5
jobtalle <jobtalle@hotmail.com>
2017-03-25 15:51:14 +0100

    Archive reads

--- include/archive/archive.h
+++ include/archive/archive.h
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <vector>
 #include <string>
+#include <cstdint>
+#include <fstream>
 
 class Archive
 {
 public:
 	Archive(const std::string path);
 	~Archive();
-private:
 
+private:
+	enum flags {
+		BATCH = 0x01,
+		CLEAN = 0x02
+	};
+
+	struct entry {
+		std::string name;
+		uint64_t offset;
+	};
+
+	uint8_t flags;
+	std::vector<entry> entries;
+	std::vector<char> payload;
+
+	void readFlags(std::ifstream &in);
+	void readEntries(std::ifstream &in);
 };
--- src/archive/archive.cpp
+++ src/archive/archive.cpp
@@ -1,13 +1,42 @@
 #include <archive/archive.h>
+#include <miniz/miniz.h>
 
 #include <stdexcept>
+#include <iostream>
 
 Archive::Archive(const std::string path)
 {
+	std::ifstream source(path, std::ios::binary);
 
+	if(!source)
+		throw std::invalid_argument("File not found");
+	else
+	{
+		readFlags(source);
+		readEntries(source);
+
+		source.close();
+	}
 }
 
 Archive::~Archive()
 {
 
+}
+
+void Archive::readFlags(std::ifstream &in)
+{
+	in.read((char*)&flags, sizeof(flags));
+}
+
+void Archive::readEntries(std::ifstream &in)
+{
+	if(flags & BATCH)
+	{
+
+	}
+	else
+	{
+
+	}
 }
